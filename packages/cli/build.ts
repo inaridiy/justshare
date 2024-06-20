6c90b11daf9b549fc5e5b606bf27a5bf9bf56ca0
inaridiy <inainainariinainari@gmail.com>
2024-06-20 22:18:59 +0900

    feat:

--- packages/cli/build.ts
+++ packages/cli/build.ts
@@ -1,85 +1,24 @@
 /*
-  For `build.ts`, further inspire @honojs/hono with inspire @kaze-style/react.
-  https://github.com/honojs/hono/blob/main/build.ts
-  https://github.com/taishinaritomi/kaze-style/blob/main/scripts/build.ts
+  This file is based on code from the following projects:
+  https://github.com/honojs/create-hono/blob/main/build.ts
+
   MIT License
-  Copyright (c) 2024 inaridiy
+  Copyright (c) 2024 Yusuke Wada <https://github.com/yusukebe>
 */
 
-import { exec } from "node:child_process";
-import fs from "node:fs";
-import path from "node:path";
-import arg from "arg";
-import { context } from "esbuild";
-import type { BuildOptions, Plugin, PluginBuild } from "esbuild";
-import { glob } from "glob";
+import { build } from "esbuild";
 
-const args = arg({
-	"--watch": Boolean,
-});
-
-const isWatch = args["--watch"];
-
-const entryPoints = glob.sync("./src/**/*.ts", {
-	ignore: ["./src/**/*.test.ts"],
-});
-
-const addExtension = (extension = ".js", fileExtension = ".ts"): Plugin => ({
-	name: "add-extension",
-	setup(build: PluginBuild) {
-		// biome-ignore lint/complexity/noExcessiveCognitiveComplexity: This is a simple plugin
-		build.onResolve({ filter: /.*/ }, (args) => {
-			if (args.importer) {
-				const p = path.join(args.resolveDir, args.path);
-				let tsPath = `${p}${fileExtension}`;
-
-				let importPath = "";
-				if (path.basename(args.importer).split(".")[0] === args.path) {
-					importPath = args.path;
-				} else if (fs.existsSync(tsPath)) {
-					importPath = args.path + extension;
-				} else {
-					tsPath = path.join(args.resolveDir, args.path, `index${fileExtension}`);
-					if (fs.existsSync(tsPath)) {
-						importPath = `${args.path}/index${extension}`;
-					}
-				}
-
-				return { path: importPath, external: true };
-			}
-		});
-	},
-});
-
-const commonOptions: BuildOptions = {
-	entryPoints,
-	logLevel: "info",
-	platform: "node",
-};
-
-const cjsBuild = () =>
-	context({
-		...commonOptions,
-		outbase: "./src",
-		outdir: "./dist/cjs",
-		format: "cjs",
-	});
-
-const esmBuild = () =>
-	context({
-		...commonOptions,
+const b = () =>
+	build({
 		bundle: true,
-		outbase: "./src",
-		outdir: "./dist",
-		format: "esm",
-		plugins: [addExtension(".js")],
+		entryPoints: ["./src/index.ts"],
+		banner: {
+			js: "#!/usr/bin/env node",
+		},
+		platform: "node",
+		outfile: "bin",
+		format: "cjs",
+		minify: true,
 	});
 
-const [esmCtx, cjsCtx] = await Promise.all([esmBuild(), cjsBuild()]);
-if (isWatch) {
-	Promise.all([esmCtx.watch(), cjsCtx.watch()]);
-} else {
-	Promise.all([esmCtx.rebuild(), cjsCtx.rebuild()]).then(() => Promise.all([esmCtx.dispose(), cjsCtx.dispose()]));
-}
-
-exec(`tsc ${isWatch ? "-w" : ""} --declaration --project tsconfig.build.json`);
+Promise.all([b()]);
