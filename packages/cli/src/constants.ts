4eb1e733c28c7262c937fd7eda9bf43ed804c971
inaridiy <inainainariinainari@gmail.com>
2024-06-23 01:58:48 +0900

    fix:

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,3 +1,3 @@
-export const PART_SIZE = 1024 * 1024 * 128; // 128MB
+export const PART_SIZE = 1024 * 1024 * 64;
 
 export const API_URL = process.env.API_URL || "https://justshare-api.inaridiy.workers.dev";
